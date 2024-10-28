93ed64520c52939431f562e242e7fc4c7a7d6030
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-10-28 12:29:01 +0530

    Add files via upload

---
+++ login_to_Application.java
@@ -0,0 +1,28 @@
+package day2;
+
+import com.microsoft.playwright.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class login_to_Application
+{
+    public static void main(String[] args)
+    {
+        Playwright pw = Playwright.create();
+        Browser brw = pw.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+        Page pg = brw.newPage();
+        pg.navigate("https://tutorialsninja.com/demo/index.php");
+        Locator loc = pg.locator("//span[text()='My Account']");
+        loc.click();
+        pg.locator("//a[text()='Login']").click();
+        assertThat(pg).hasTitle("Account Login");
+        pg.getByPlaceholder("E-Mail Address").type("amotooricap8@gmail.com");
+        pg.getByPlaceholder("Password").type("12345");
+        pg.locator("//*[@value='Login']").click();
+        assertThat(pg).hasTitle("My Account");
+        pg.close();
+        brw.close();
+        pw.close();
+    }
+}
