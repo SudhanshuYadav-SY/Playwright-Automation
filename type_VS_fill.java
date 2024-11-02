aff93c96cf415585ae9b17d52ba6f26981ce67d9
Sudhanshu Yadav <yadav.sy.sudhanshu@gmail.com>
2024-11-03 02:09:44 +0530

    Add files via upload

--- type_VS_fill.java
+++ type_VS_fill.java
@@ -15,8 +15,14 @@ public class type_VS_fill
         );
         Page pg = brw.newPage();
         pg.navigate("https://www.lambdatest.com/selenium-playground/generate-file-to-download-demo");
-        //This is an example of Typing on Web Page using "type"
+        /*
+        This is an example of Typing on Web Page using "type"
         pg.locator("#textbox").type("A random paragraph can also be an excellent way for a writer to tackle writers' block. Writing block can often happen due to being stuck with a current project that the writer is trying to complete. By inserting a completely random paragraph from which to begin, it can take down some of the issues that may have been causing the writers' block in the first place.");
+        */
+        /*
+        This is an example of Typing on Web Page using "fill"
+        pg.locator("#textbox").fill("A random paragraph can also be an excellent way for a writer to tackle writers' block. Writing block can often happen due to being stuck with a current project that the writer is trying to complete. By inserting a completely random paragraph from which to begin, it can take down some of the issues that may have been causing the writers' block in the first place.");
+        */
         pg.close();
         brw.close();
         pw.close();
